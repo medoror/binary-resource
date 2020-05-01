98f2500cd3fddf90a0e8e46141e6478e8cd2cc69
Michael Edoror <medoror@gmail.com>
2020-04-30 21:48:43 -0700

    Continues In Script

--- spec/in_spec.rb
+++ spec/in_spec.rb
@@ -1,13 +1,58 @@
 require_relative '../src/in'
+require 'fakefs/safe'
 
 describe "in" do
-  context "#download_binary" do
-    it "downloads binary arrording to given version" do
-
+  payload = JSON.parse("{
+            \"source\": {
+                \"repo\": \"pivotal-cf/om\",
+                \"tag\": \"4.6.0\"
+            },
+            \"version\": {
+                \"version\": \"4.6.0\"
+            }
+        }")
+  in_script = In.new(payload)
+  context "initialize" do
+    it "pulls version from stdin" do
+      expect(in_script.version).to eq "4.6.0"
     end
+  end
+  context "get_download_link" do
+    it "get download link given a release" do
+      expect(in_script.get_download_link(
+          JSON.parse(File.read(File.join(__dir__, "fixtures", "om-single-tag-release.json")
+      )))).to eq "https://github.com/pivotal-cf/om/releases/download/4.6.0/om-linux-4.6.0.tar.gz"
+    end
+    it "raises error if download link could not be retrieved" do
+      expect{ in_script.get_download_link(nil) }
+          .to raise_error "Download Link could not be retrieved"
+      expect{ in_script.get_download_link(JSON.parse(File.read(
+          File.join(__dir__, "fixtures", "om-single-tag-release-no-assets.json")))) }
+          .to raise_error "Download Link could not be retrieved"
+      expect{ in_script.get_download_link(JSON.parse(File.read(
+          File.join(__dir__, "fixtures", "om-single-tag-release-no-tgz.json")))) }
+          .to raise_error "Download Link could not be retrieved"
+    end
+  end
+  # context "#download_binary" do
+  #   it "downloads binary according to given version" do
+  #     # expect(Net::HTTP).to receive(:get_response)
+  #     #                          .with("").and_return(nil)
+  #     # in_script.download_version
+  #
+  #   end
+  #
+  #   it "raises error if download fails" do
+  #
+  #   end
+  # end
 
-    it "raises error if download fails" do
-
+  context "#create_dest_dir" do
+    it "creates given destination directory" do
+      FakeFS do
+        expect(FileUtils).to receive(:mkdir_p).with("tmp").once
+        in_script.create_dest_dir("tmp")
+      end
     end
   end
 end
--- src/in.rb
+++ src/in.rb
@@ -1,5 +1,10 @@
 #!/usr/bin/env ruby
 require_relative './payload'
+require 'net/http'
+require 'uri'
+require 'json'
+require 'tty-command'
+
 class In < Payload
 
   # STDIN:
@@ -11,13 +16,67 @@ class In < Payload
   # }
   #
   # STDOUT
+  attr_accessor :version
 
-  def main
-    # read version
-    # download the given version
-    # unpack tar and place in in bin dir
+  def initialize(payload)
+    super
+    @version = payload["version"]["version"]
+  end
+
+  def create_dest_dir(destinator_dir)
+    FileUtils.mkdir_p destinator_dir
+    File.join(destinator_dir, @repo)
+  end
+
+  def get_download_link(payload)
+    # explicitly for linux until we inject distro choice from receipe
+    begin
+      payload["assets"].find { |asset| asset["name"].match?(/.*linux.*gz/)}["browser_download_url"]
+    rescue NoMethodError => e
+      raise "Download Link could not be retrieved"
+    end
+  end
+
+  def get_release_by_version
+    uri = URI.parse("https://api.github.com/repos/#{@owner}/#{@repo}/releases/tags/#{@version}")
+    JSON.parse(Net::HTTP.get_response(uri))
+  end
+
+  def main(destination_dir)
+    response = get_release_by_version
+
+    download_link = get_download_link(response)
+    download_name = download_link.match(/([^\/]+).gz/)[0]
+
+    # download the binary
+    wget(download_link, download_name)
+
+    full_destination = create_dest_dir(destination_dir)
+
+    untar_download_to_destination(download_name, full_destination)
+
+  end
+  private
+
+  def untar_download_to_destination(download_name, full_destination)
+    cmd = TTY::Command.new
+    cmd.run("tar -xvfz #{download_name} -C #{full_destination}")
+  end
+
+  def wget(url,file)
+    unless file
+      file = File.basename(url)
+    end
+
+    url = URI.parse(url)
+    Net::HTTP.start(url.host) do |http|
+      resp = http.get(url.path)
+      open(file, "wb") do |file|
+        file.write(resp.body)
+      end
+    end
   end
 end
 
 
-# In.new(JSON.parse(ARGF.read)).main
+# In.new(JSON.parse(ARGF.read)).main(ARGV[0])
