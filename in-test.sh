2595aa327aaeab97f02d319a1126616903dec20a
Michael Edoror <medoror@gmail.com>
2020-05-03 22:15:23 -0700

    Deletes tmp dir every run

--- in-test.sh
+++ in-test.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+DEST_DIR="tmp/"
+if [ -d "$DEST_DIR" ]; then
+    rm -Rf $DEST_DIR;
+    echo "Deleting destination dir..."
+fi
+
 ruby src/in.rb "tmp/"  <<EOF
 {
    "source": {
